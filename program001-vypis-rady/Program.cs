6c7033d46aa54b37534fea168a6d09bcea3ebb2b
LucieBartova <lucie.bartova@tul.cz>
2025-10-01 10:31:34 +0200

    Add files via upload

---
+++ program001-vypis-rady/Program.cs
@@ -0,0 +1,29 @@
+﻿string again = "a";
+while (again == "a")
+{
+    Console.Clear();
+    Console.WriteLine("********************");
+    Console.WriteLine("lucinka");
+    Console.WriteLine("pusinka");
+    Console.WriteLine("********************");
+    Console.WriteLine();
+
+    // Vstup hodnoty do programu - špatně řešení
+    //Console.Write("Zadejte první číslo řady: ");
+    //int first = int.Parse(Console.ReadLine());
+
+    //Vstup hodnoty do programu - řešený správně
+    Console.WriteLine("Zadejte první číslo řady (celé číslo)");
+int first;
+
+    while (!int.TryParse(Console.ReadLine(), out first));
+    {
+        Console.Write("Nezadali jste celé číslo. Zadejte prví číslo řady znovu:");
+}
+
+
+
+        Console.WriteLine();
+    Console.WriteLine("Pro opakovani programu stisknete klavesu a.");
+    again = Console.ReadLine();
+}
